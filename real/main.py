3007c705a30e8694e53f594fcfd94b4048dc89ef
Hyeongwon-Jang <janghw0911@kaist.ac.kr>
2025-05-25 03:57:40 +0900

    Adjusting precision for OOM

--- real/main.py
+++ real/main.py
@@ -307,6 +307,7 @@ def main(
                 save_dir=".",
                 version=random.getrandbits(128),
             ),
+            # precision=16, # if OOM occurs
         )
         mask = GateMaskNet(
             forward_func=classifier.predict,
